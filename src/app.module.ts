5d052bcb3e68fbc139fada09b2bbb0520fb14f5b
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2023-02-17 08:24:24 -0300

    feat: generate customer

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { AppService } from './app.service';
       entities: [],
       synchronize: true,
     }),
+    CustomerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
