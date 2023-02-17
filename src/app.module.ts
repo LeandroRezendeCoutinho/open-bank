068c7d82e1011cebe756c088c633131e26368878
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2023-02-17 08:36:56 -0300

    fix: eslint semi never

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CustomerModule } from './customer/customer.module';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { CustomerModule } from './customer/customer.module'
 
 @Module({
   imports: [
--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,7 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CustomerService } from './customer.service';
-import { CreateCustomerDto } from './dto/create-customer.dto';
-import { UpdateCustomerDto } from './dto/update-customer.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common'
+import { CustomerService } from './customer.service'
+import { CreateCustomerDto } from './dto/create-customer.dto'
+import { UpdateCustomerDto } from './dto/update-customer.dto'
 
 @Controller('customer')
 export class CustomerController {
@@ -9,26 +9,26 @@ export class CustomerController {
 
   @Post()
   create(@Body() createCustomerDto: CreateCustomerDto) {
-    return this.customerService.create(createCustomerDto);
+    return this.customerService.create(createCustomerDto)
   }
 
   @Get()
   findAll() {
-    return this.customerService.findAll();
+    return this.customerService.findAll()
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.customerService.findOne(+id);
+    return this.customerService.findOne(+id)
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCustomerDto: UpdateCustomerDto) {
-    return this.customerService.update(+id, updateCustomerDto);
+    return this.customerService.update(+id, updateCustomerDto)
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.customerService.remove(+id);
+    return this.customerService.remove(+id)
   }
 }
