8ffef788e20338130496ffe3f45883ac34e83cd7
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2023-02-27 08:10:59 -0300

    feat: add users module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CustomerModule } from './customer/customer.module'
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     CustomerModule,
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
