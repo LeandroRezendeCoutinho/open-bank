42f533a7c0555d1b2b3d9557582dfc9e31bc2b01
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2023-02-27 08:09:41 -0300

    feat: add auth module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { CustomerModule } from './customer/customer.module'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { CustomerModule } from './customer/customer.module'
       synchronize: true,
     }),
     CustomerModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
